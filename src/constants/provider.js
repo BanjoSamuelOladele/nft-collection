6cb16ebe516d6b64a0b98a97398e51daeb2b7f96
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-15 06:44:46 +0100

    created the web socket provider

--- src/constants/provider.js
+++ src/constants/provider.js
@@ -1,6 +1,12 @@
 import { ethers } from "ethers";
 
 
-export const readOnly = new ethers.JsonRpcProvider(
+export const rpcReadOnly = new ethers.JsonRpcProvider(
     import.meta.env.VITE_rpc_call
 )
+
+
+export const webSocketReadOnly = new ethers.WebSocketProvider(
+    import.meta.env.VITE_webSocket
+);
+
