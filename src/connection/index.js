2a6621a1415636e9f0259160aa2df3cfacef18d3
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-15 05:21:29 +0100

    created the env file

---
+++ src/connection/index.js
@@ -0,0 +1,26 @@
+
+
+
+// 2. Set chains
+const mainnet = {
+    chainId: 1,
+    name: 'Ethereum',
+    currency: 'ETH',
+    explorerUrl: 'https://etherscan.io',
+    rpcUrl: 'https://cloudflare-eth.com'
+}
+
+const metadata = {
+    name: 'My Website',
+    description: 'My Website description',
+    url: 'https://mywebsite.com', // origin must match your domain & subdomain
+    icons: ['https://avatars.mywebsite.com/']
+}
+
+createWeb3Modal({
+    ethersConfig,
+    chains: [mainnet],
+    projectId,
+    enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
