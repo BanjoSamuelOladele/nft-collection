b79d79256e55140ea5f5a9770175f7a101da4b14
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-15 05:49:12 +0100

    rendered the Header function

--- src/functions/Header.jsx
+++ src/functions/Header.jsx
@@ -1,10 +1,16 @@
+import { Flex } from "@radix-ui/themes";
 
 
 
 
 const Header = () => {
     return(
-        <></>
+        <div className="flex justify-between items-center">
+            <div>Echelon NFT Collection</div>
+            <Flex gap={"4"} align={"center"}>
+                <w3m-button />
+            </Flex>
+        </div>
     )
 }
 
