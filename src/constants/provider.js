380b365a337f0a2b92401ae8100428cebdb3b683
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-15 06:46:25 +0100

    created the browserProvider option

--- src/constants/provider.js
+++ src/constants/provider.js
@@ -3,10 +3,12 @@ import { ethers } from "ethers";
 
 export const rpcReadOnly = new ethers.JsonRpcProvider(
     import.meta.env.VITE_rpc_call
-)
+);
 
 
 export const webSocketReadOnly = new ethers.WebSocketProvider(
     import.meta.env.VITE_webSocket
 );
 
+
+export const getProvider = (provider) => new ethers.BrowserProvider(provider);
